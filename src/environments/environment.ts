b9a4d28a91198cde83a41cecd5a478890f4af861
Sergei Kuzminsky <sergei@matchpoker.com>
2024-03-11 14:59:31 -0300

    vps url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://172.81.181.19:9191',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://172.81.181.19:9191',
+  //apiUrl: 'http://localhost:3000',
 };
